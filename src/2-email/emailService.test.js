66aa38f351c2cd46a78138e2f808d48e327fea40
daniellferreira <daniel.lopes.ferreira98@gmail.com>
2024-11-25 18:00:52 -0300

    adiciona mais uma regra

--- src/2-email/emailService.test.js
+++ src/2-email/emailService.test.js
@@ -6,4 +6,8 @@ jest.mock('./emailClient');
 test('envia o email com os parametros corretos', () => {
     enviarEmail('destination@email.com', 'Olá Mundo');
     expect(emailClient.send).toHaveBeenCalledWith('email@email.com', 'destination@email.com', 'Olá Mundo');
+    expect(emailClient.send).toHaveBeenCalledTimes(1);
 })
+
+
+
